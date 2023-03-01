f783d83c510a4b9a284305d7b24a798da07acf1c
Grayson Miller <grayson.miller124@gmail.com>
2023-02-28 20:09:55 -0700

    Working ProjectCreator.new and .run

--- poetspen/__main__.py
+++ poetspen/__main__.py
@@ -1,13 +1,46 @@
 import logging
+import argparse
+
+from pathlib import Path
+
 
 from .project_creator import ProjectCreator
 
 
-def main():
-    logging.basicConfig(level=logging.DEBUG, format="[%(levelname)s]\t%(message)s")
+def parser():
+    parser = argparse.ArgumentParser()
+    # command group
+    subparsers = parser.add_subparsers(dest="command", required=True)
+
+    # new command
+    new_parser = subparsers.add_parser("new", help="Create a new project")
+    new_parser.add_argument("-rp", "--root_path", help="The path to create the project in", default=Path.cwd())
+    new_parser.add_argument("-r", "--recreate", help="Recreate the project if it already exists", action="store_true", default=False)
+    new_parser.add_argument("project_name", help="The name of the project to create")
+
+    # run command
+    run_parser = subparsers.add_parser("run", help="Run a project")
+    run_parser.add_argument("-rp", "--root_path", help="The path where the project is located", default=Path.cwd())
     
+    # add dep / remove dep / update dep
+    # install / uninstall / update lockfile
+
+    return parser
+
+
+def main():
+    args = parser().parse_args()
+    logging.basicConfig(level=logging.DEBUG, format="[%(levelname)s]\t%(message)s")
+
     pc = ProjectCreator()
-    pc.create("asdf")
+
+    root_path = Path(args.root_path) / "demo"
+
+    match args.command:
+        case "new":
+            pc.new(root_path, args.project_name, recreate=args.recreate)
+        case "run":
+            pc.setup(root_path).run()
 
 
 if __name__ == "__main__":
--- poetspen/project_creator.py
+++ poetspen/project_creator.py
@@ -1,38 +1,78 @@
 import logging
+import os
+
 from pathlib import Path
+import shutil
+from subprocess import run
+from typing import Literal
 
 from poetry.factory import Factory
 from poetry.installation.installer import Installer
 from poetry.utils.env import EnvManager
-from cleo.io.buffered_io import BufferedIO
 
+from cleo.io.buffered_io import BufferedIO
 
 logger = logging.getLogger(__name__)
 
 
 class ProjectCreator:
     def __init__(self) -> None:
-        pass
+        self._poetry = None
+        self._io = None
+        self._installer = None
 
-    @staticmethod
-    def create(project_path: str) -> None:
-        # this man is a godsend: https://github.com/python-poetry/poetry/issues/1053#issuecomment-935758339
-        poetry = Factory().create_poetry(Path(project_path))
-
-        ioo = BufferedIO()
-        env = EnvManager(poetry).create_venv(ioo)
-        logger.debug(f"Using env: {env.path}")
-
+    def setup(self, project_path: Path):
+        # see: https://github.com/python-poetry/poetry/issues/1053#issuecomment-935758339
+        poetry = Factory().create_poetry(project_path)
+        io = BufferedIO()
         installer = Installer(
-            ioo,
-            env,
+            io,
+            EnvManager(poetry).create_venv(io),
             poetry.package,
             poetry.locker,
             poetry.pool,
             poetry.config,
         )
 
-        installer.update(True)
-        installer.run()
+        self._poetry = poetry
+        self._io = io
+        self._installer = installer
 
-        print(ioo.fetch_output())
+        logger.info(f"Initialized Poetry project in {project_path}! Project name: {poetry.package.name}")
+
+        return self
+
+    @staticmethod
+    def new(root_path: Path, project_name: str, recreate: bool = False) -> 'ProjectCreator':
+        # if a project already exists, don't create it
+        project_path = root_path / project_name
+        if not recreate:
+            if (project_path / "pyproject.toml").exists():
+                logger.error(f"Project already exists at {project_path}!")
+                return
+        else:
+            if project_path.exists():
+                logger.warning(f"Removing existing project at {project_path}!")
+                shutil.rmtree(project_path)
+
+
+        if not root_path.exists():
+            root_path.mkdir(parents=True)
+
+        # TODO: Do this properly with the poetry API.
+        results = run(["poetry", "new", project_name], cwd=root_path)
+
+        if results.returncode != 0:
+            logger.error("Failed to create project with poetry!")
+            return
+        
+        return ProjectCreator().setup(project_path)
+
+    def run(self):
+        results = run(["poetry", "run"], cwd=self._poetry.file.parent)
+
+    def install(self, update: bool = False) -> None:
+        if update:
+            self._installer.update(True)
+            
+        self._installer.run()
